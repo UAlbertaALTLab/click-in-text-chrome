6b1b26da1faca089fc96d987b4d4fcfc025954b2
Eddie Antonio Santos <easantos@ualberta.ca>
2019-07-09 15:18:06 -0600

    Convert contentscript to ASCII on build.

---
+++ libexec/askiy.js
@@ -0,0 +1,22 @@
+/**
+ * Turns all non-ASCII characters in a UTF-8 file into \uXXXX escapes.
+ */
+
+const fs = require('fs');
+
+let [_node, _script, infile, outfile] = process.argv;
+
+let fileContents = fs.readFileSync(infile, 'utf8');
+
+let lines = fileContents.split('\n');
+let better = [];
+
+for (let line of lines) {
+  better.push(line.replace(/[\u0080-\uFFFF]/g, function (c) {
+    let hex = c.charCodeAt(0).toString(16);
+    let zeros = '0'.repeat(4 - hex.length);
+    return '\\u' + zeros + hex;
+  }));
+}
+
+fs.writeFileSync(outfile, better.join('\n'), 'utf8');
