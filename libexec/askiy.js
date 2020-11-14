20ec143d7b1ad61df50190d9d0f193fb5395503c
matt <syan4@ualberta.ca>
2020-11-13 19:59:41 -0700

    eslint fix - unused vars

--- libexec/askiy.js
+++ libexec/askiy.js
@@ -2,21 +2,21 @@
  * Turns all non-ASCII characters in a UTF-8 file into \uXXXX escapes.
  */
 
-const fs = require('fs');
+const fs = require('fs')
 
-let [_node, _script, infile, outfile] = process.argv;
+let [, , infile, outfile] = process.argv
 
-let fileContents = fs.readFileSync(infile, 'utf8');
+let fileContents = fs.readFileSync(infile, 'utf8')
 
-let lines = fileContents.split('\n');
-let better = [];
+let lines = fileContents.split('\n')
+let better = []
 
 for (let line of lines) {
   better.push(line.replace(/[\u0080-\uFFFF]/g, function (c) {
-    let hex = c.charCodeAt(0).toString(16);
-    let zeros = '0'.repeat(4 - hex.length);
-    return '\\u' + zeros + hex;
-  }));
+    let hex = c.charCodeAt(0).toString(16)
+    let zeros = '0'.repeat(4 - hex.length)
+    return '\\u' + zeros + hex
+  }))
 }
 
-fs.writeFileSync(outfile, better.join('\n'), 'utf8');
+fs.writeFileSync(outfile, better.join('\n'), 'utf8')
