3e99226c7c58e6320a2d525c9f46a1696564f3df
matt <syan4@ualberta.ca>
2020-12-27 01:56:49 -0700

    wait for longer

--- cypress/integration/type_and_translate_popup.ts
+++ cypress/integration/type_and_translate_popup.ts
@@ -4,7 +4,7 @@ describe('Popup Existence', function() {
     cy.visit('localhost:8080/test.html')
 
     // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(500) // if the click happens too fast, the javascript won't have time to register the tat popup
+    cy.wait(2000) // if the click happens too fast, the javascript won't have time to register the tat popup
     cy.get('#tat_button').click()
 
     cy.get('transover-type-and-translate-popup')
