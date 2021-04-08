9a07c1cfa71a88af2ab9157de3fcbaa9bfa944f8
Andrew Neitsch <andrew@neitsch.ca>
2021-04-08 09:52:32 -0600

    Make test-server script behave a little better

--- libexec/test-server.ts
+++ libexec/test-server.ts
@@ -3,11 +3,15 @@
  */
 
 import {spawn} from "child_process";
+import {existsSync} from "fs";
 
 
 export function startServer(onClose: (...args) => unknown):void {
+  if (!existsSync('dist/test.html')) {
+    throw new Error("dist/test.html not found; do you need to run a build first?")
+  }
 
-  spawn('npx', ['http-server', 'dist'], {shell: true})
+  spawn('npx', ['http-server', 'dist', '-p', '8080'], {shell: true})
   const waitOnProcess = spawn('npx', ['wait-on', 'http-get://localhost:8080/test.html'], {shell: true})
   waitOnProcess.on('close', onClose)
 }
